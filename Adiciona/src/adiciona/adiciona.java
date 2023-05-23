87d90edca245bffff230e7475cb4e317a33e265a
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2023-05-23 11:12:27 -0300

    Att

---
+++ Adiciona/src/adiciona/adiciona.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adiciona;
+
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author vitor.felicio
+ */
+class adiciona {
+    public static void main(String[] args) {
+        // Cria uma nova instância do JFrame
+        JFrame frame = new JFrame("Meu JFrame");
+        
+        // Configurações do JFrame
+        frame.setSize(500, 500);  // Define o tamanho do JFrame em pixels
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Define o comportamento padrão ao fechar o JFrame
+        
+        // Define a visibilidade do JFrame como verdadeira
+        frame.setVisible(true);
+    }
+}
